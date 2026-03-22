7c117c43a0cdd53727196a31c86c3d699aef1e47
Александр <vka2@list.ru>
2026-03-22 21:49:52 +0700

    Add error handling for 400, 404, and 500 statuses

--- practice2/src/api/threatsApi.js
+++ practice2/src/api/threatsApi.js
@@ -12,7 +12,13 @@ api.interceptors.response.use(
   error => {
     if (error.response?.status === 401) {
       console.warn('Не авторизован (401). Требуется аутентификация.');
-      
+    else if (error.response?.status === 400) {
+      console.warn('Не корректный запрос (400). Проверьте данные.');
+    else if (error.response?.status === 404) {
+      console.warn('Запрашиваемый ресурс не найден.');
+    else if (error.response?.status === 500) {
+      console.warn('Внутренняя ошибка сервера (500). Попробуйте позже.');
+    
     }
     return Promise.reject(error);
   }
